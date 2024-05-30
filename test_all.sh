bdee15357a2e238274cb995fca8bf9d53cfcaf79
pgleeson <p.gleeson@gmail.com>
2024-05-30 18:28:22 +0100

    More examples

--- test_all.sh
+++ test_all.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -ex
 
-pynml -validate *nml
+pynml -validate C*nml *cell.nml *channel.nml 
+
+pynml-summary pyr_soma_m_in_b_in.cell.nml -v
+pynml-summary pyr_soma_m_in_b_out.cell.nml -v
+pynml-summary pyr_soma_m_out_b_in.cell.nml -v
+pynml-summary pyr_soma_m_out_b_out.cell.nml -v
+
+pynml LEMS_m_in_b_in.xml -nogui
+pynml LEMS_m_in_b_in.xml -neuron -run -nogui
+pynml LEMS_m_in_b_out.xml -neuron -run -nogui
+pynml LEMS_m_out_b_in.xml -neuron -run -nogui
+pynml LEMS_m_out_b_out.xml -neuron -run -nogui
