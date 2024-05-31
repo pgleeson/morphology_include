9ef67e8c793bc265b66e5d88c36abc116a6b2ab0
pgleeson <p.gleeson@gmail.com>
2024-05-31 10:39:42 +0100

    More tests with netpyne

--- test_all.sh
+++ test_all.sh
@@ -9,7 +9,13 @@ pynml-summary pyr_soma_m_out_b_in.cell.nml -v
 pynml-summary pyr_soma_m_out_b_out.cell.nml -v
 
 pynml LEMS_m_in_b_in.xml -nogui
+
 pynml LEMS_m_in_b_in.xml -neuron -run -nogui
 pynml LEMS_m_in_b_out.xml -neuron -run -nogui
 pynml LEMS_m_out_b_in.xml -neuron -run -nogui
 pynml LEMS_m_out_b_out.xml -neuron -run -nogui
+
+pynml LEMS_m_in_b_in.xml -netpyne -run
+pynml LEMS_m_in_b_out.xml -netpyne -run
+pynml LEMS_m_out_b_in.xml -netpyne -run
+pynml LEMS_m_out_b_out.xml -netpyne -run
